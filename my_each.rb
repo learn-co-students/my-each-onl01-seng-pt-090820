17aa9c3df341084bf74b6157984acc6207a91609
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-14 19:37:26 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) 
+  i = 0 
+  if i < array.length
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+  else
+  end
+  array
 end
+
