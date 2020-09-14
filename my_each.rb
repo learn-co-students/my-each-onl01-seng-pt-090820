245a26199848f3496cfc10f8bb2b5c9aabac5f3e
Kristian Joos <kjoos22@gmail.com>
2020-09-14 22:11:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(input)
+  x = 0
+  if input.size > 0
+    while x < input.size
+      yield input[x]
+      x = x + 1
+    end
+  end
+  input
 end
