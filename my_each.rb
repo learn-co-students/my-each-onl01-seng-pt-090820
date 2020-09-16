5032dbc0da2b0a91d161b0c36b7fb40f116ae53d
Chase090 <carloyam21@icloud.com>
2020-09-16 14:10:52 -0700

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+
+require 'pry'
+def my_each(collection) # put argument(s) here
+  counter = 0
+  while counter < collection.length
+   yield (collection[counter])
+   counter += 1
+  end
+  return collection
 end
+binding.pry
