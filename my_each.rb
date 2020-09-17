0ba6c27372bdee846eb8bd00b6c6649903faebc6
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-17 18:48:55 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,21 +1,8 @@
 def my_each(array)
-  i = 0
+  i = 0  
   while i < array.length
     yield(array[i])
     i = i + 1
   end
   array
 end
-
-
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-# hello(list) {|i| "Hello #{i} what's up?"}
